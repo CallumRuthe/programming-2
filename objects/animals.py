a3b9951b4db1f5a48cedecb7099c6e84c18447dc
Callum Ruthe <CR000052@sd38.bc.ca>
2021-01-08 11:28:36 -0800

    created animals.py (looking into objects)

---
+++ objects/animals.py
@@ -0,0 +1,32 @@
+# animals.py
+# a look at object oriented programming
+
+class Animal():
+    # CONSTRUCTOR
+    def __init__(self):
+        self.legs = 0
+        self.hairy = False
+
+    def talk(self):
+        return "Hello."
+
+    def eat(self, food):
+        if food == "meat":
+            return "Yummmmm. Meat."
+        elif food in ["veggies", "vegetables"]:
+            return "Yummmmm. Veggies"
+        elif food == "poison":
+            return "I'm not eating that."
+        else:
+            return "Poop."
+
+
+dog = Animal()
+dog.legs = 4
+dog.hairy = True
+
+# if dog.hairy:
+#     print("You'll need to brush its hair.")
+
+print(dog.talk())
+print(dog.eat("meat"))
